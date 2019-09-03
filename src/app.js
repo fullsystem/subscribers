1875f947946fddd8306de052aa187e1476b7f33c
Renato Dehnhardt <renatotkd@gmail.com>
2019-09-03 11:46:45 +0200

    Revert "First Commit"
    This reverts commit ed661135

--- src/app.js
+++ src/app.js
@@ -1,32 +1,17 @@
-'use strict'
+let response;
 
-exports.lambdaHandler = async (event, context, callback) => {
-    const done = (err, res) => callback(null, {
-        statusCode: err ? '400' : '200',
-        body: err ? err.message : JSON.stringify(res),
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    })
-
-    switch (event.httpMethod) {
-        case 'DELETE':
-            // dynamo.deleteItem(JSON.parse(event.body), done)
-            return done(null, JSON.parse(event.body))
-            break;
-        case 'GET':
-            // dynamo.scan({ TableName: event.queryStringParameters.TableName }, done)
-            return done(null, JSON.parse(event.body))
-            break;
-        case 'POST':
-            // dynamo.putItem(JSON.parse(event.body), done)
-            return done(null, JSON.parse(event.body))
-            break;
-        case 'PUT':
-            // dynamo.updateItem(JSON.parse(event.body), done)
-            return done(null, JSON.parse(event.body))
-            break;
-        default:
-            return done(new Error(`Unsupported method "${event.httpMethod}"`))
+exports.lambdaHandler = async (event, context) => {
+    try {
+        response = {
+            'statusCode': 200,
+            'body': JSON.stringify({
+                message: 'Subscribers',
+            })
+        }
+    } catch (err) {
+        console.log(err);
+        return err;
     }
-}
+
+    return response
+};
