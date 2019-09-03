ed661135240aec5687d840ee4ec1f5170bf43c18
Renato Dehnhardt <renatotkd@gmail.com>
2019-09-03 10:42:32 +0200

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,17 +1,32 @@
-let response;
+'use strict'
 
-exports.lambdaHandler = async (event, context) => {
-    try {
-        response = {
-            'statusCode': 200,
-            'body': JSON.stringify({
-                message: 'Subscribers',
-            })
-        }
-    } catch (err) {
-        console.log(err);
-        return err;
+exports.lambdaHandler = async (event, context, callback) => {
+    const done = (err, res) => callback(null, {
+        statusCode: err ? '400' : '200',
+        body: err ? err.message : JSON.stringify(res),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+
+    switch (event.httpMethod) {
+        case 'DELETE':
+            // dynamo.deleteItem(JSON.parse(event.body), done)
+            done(null, JSON.parse(event.body))
+            break;
+        case 'GET':
+            // dynamo.scan({ TableName: event.queryStringParameters.TableName }, done)
+            done(null, JSON.parse(event.body))
+            break;
+        case 'POST':
+            // dynamo.putItem(JSON.parse(event.body), done)
+            done(null, JSON.parse(event.body))
+            break;
+        case 'PUT':
+            // dynamo.updateItem(JSON.parse(event.body), done)
+            done(null, JSON.parse(event.body))
+            break;
+        default:
+            done(new Error(`Unsupported method "${event.httpMethod}"`))
     }
-
-    return response
-};
+}
