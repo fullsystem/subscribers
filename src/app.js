2c6bc38a7731431b2bd395e9a3f29e7e7b38ea27
Renato Dehnhardt <renatotkd@gmail.com>
2019-09-03 11:56:07 +0200

    Application file definition

--- src/app.js
+++ src/app.js
@@ -1,17 +1,18 @@
 let response;
 
-exports.lambdaHandler = async (event, context) => {
+exports.lambdaHandler = async (event, context, callback) => {
+    const response = (err, res) => callback(null, {
+        statusCode: err ? '400' : '200',
+        body: err ? err.message : JSON.stringify(res),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+
     try {
-        response = {
-            'statusCode': 200,
-            'body': JSON.stringify({
-                message: 'Subscribers',
-            })
-        }
+        response(null, { message: 'Subscribers' })
     } catch (err) {
         console.log(err);
-        return err;
+        response(new Error(err))
     }
-
-    return response
 };
