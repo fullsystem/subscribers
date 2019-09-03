449b9ed377662f5f79605fb2cc0e7cd091e95560
Renato Dehnhardt <renatotkd@gmail.com>
2019-09-03 12:53:40 +0200

    Application file definition

--- src/app.js
+++ src/app.js
@@ -1,18 +1,32 @@
-let response;
+'use strict'
+
+const subscribers = require("./subscribers")
 
 exports.lambdaHandler = async (event, context, callback) => {
-    const response = (err, res) => callback(null, {
+    const response = (err, response) => callback(null, {
         statusCode: err ? '400' : '200',
-        body: err ? err.message : JSON.stringify(res),
+        body: err ? JSON.stringify({error: err.message}) : JSON.stringify(response),
         headers: {
             'Content-Type': 'application/json',
         },
     })
 
     try {
-        response(null, { message: 'Subscribers' })
-    } catch (err) {
-        console.log(err);
-        response(new Error(err))
+        switch (event.httpMethod) {
+            case 'GET':
+                await subscribers.search(JSON.parse(event.body), response)
+                break;
+            case 'POST':
+                await subscribers.create(JSON.parse(event.body), response)
+                break;
+            case 'PUT':
+                await subscribers.update(JSON.parse(event.body), response)
+                break;
+            default:
+                response(new Error(`Unsupported method "${event.httpMethod}"`))
+        }
+    } catch (error) {
+        console.log(error)
+        response(new Error(error))
     }
 };
---
+++ src/subscribers.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const AWS = require("aws-sdk")
+
+exports.search = (params, callback) => {
+    callback(null, params)
+};
+
+exports.create = (params, callback) => {
+    callback(null, params)
+};
+
+exports.update = (params, callback) => {
+    callback(null, params)
+};
