88057f57e3afb7f2430db1d917ccd87191e9a61f
Renato Dehnhardt <renatotkd@gmail.com>
2019-09-04 09:24:44 +0200

    Try to have a response

--- src/subscribers.js
+++ src/subscribers.js
@@ -11,30 +11,7 @@ module.exports.search = (data, callback) => {
 module.exports.create = (data, callback) => {
     const timestamp = new Date().getTime()
 
-    if (typeof data.email !== 'string') {
-        callback(new Error('Email is required.'))
-        return
-    }
-
-    const params = {
-        TableName: process.env.SUBSCRIBERS_TABLE,
-        Item: {
-            id: uuid.v1(),
-            email: data.email,
-            activated_at: false,
-            createdAt: timestamp,
-            updatedAt: timestamp,
-        },
-    }
-
-    db.put(params, (error) => {
-        if (error) {
-            callback(new Error('Couldn\'t create the subscriber.'))
-            return
-        }
-
-        callback(null, params.Item)
-    })
+    callback(null, {date: timestamp})
 }
 
 exports.update = (params, callback) => {
